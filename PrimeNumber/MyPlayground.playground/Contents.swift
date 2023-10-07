fa443edceaec97fbe87ee1c84d0975c52beec1ae
“rockVivek” <vinodkumarshrivastawa@gmail.com>
2023-10-07 20:13:05 +0530

    Adding code for finding weather a number is prime number or not

---
+++ PrimeNumber/MyPlayground.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+import Foundation
+
+//prime number
+
+func isPrimeNumber(number: Int) -> Bool{
+    for i in 2 ... Int(sqrt(Double(number))) {
+        if number % i == 0{
+            return false
+        }
+    }
+    return true
+}
+
+let number = 78 
+let isPrime = isPrimeNumber(number: number)
+print("the given number \(number) is prime: \(isPrime)")
+
+
+
+
+
+
